1c1ca3365031b078ad7f22c52c020c19d78fe3f3
Belly_Musketier <32067335+Tatya-winchu@users.noreply.github.com>
2020-06-17 00:46:53 +0200

    removing unnecessary comments

--- playground/json.js
+++ playground/json.js
@@ -1,17 +1,8 @@
-// var obj = {
-//   name: 'Kedar'
-// };
-// var stringObj = JSON.stringify(obj);
-// console.log(typeof stringObj);
-// console.log(stringObj);
-// var personString = '{"name": "Kedar", "age":"26"}';
-// var person = JSON.parse(personString);
-// console.log (typeof person);
-// console.log (person);
+
 const fs = require ('fs');
 
 var orignalNote = {
-  title: 'Kedar',
+  title: 'Tatya',
   body: 'Data'
 };
 var originalNoteString = JSON.stringify(orignalNote);
