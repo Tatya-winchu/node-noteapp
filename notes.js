0276aab377ea800d77f8118d524f9b27bc2c3d1e
Belly_Musketier <32067335+Tatya-winchu@users.noreply.github.com>
2020-06-17 00:49:54 +0200

    var to let! Its always better this way

--- notes.js
+++ notes.js
@@ -2,55 +2,55 @@ console.log('Starting Notes.js');
 const fs = require('fs');
 
 //fetching th duplicateNotes
-var fetchNotes = () => {
+let fetchNotes = () => {
   //reading the previous note from an array
   try {
-    var noteSting =   fs.readFileSync('notes-data.json');
+    let noteSting =   fs.readFileSync('notes-data.json');
     return JSON.parse(noteSting);
   } catch (e) {
    return []; //if try fails it will return emptey array
   }
 };
-var saveNotes = (notes) => {
+let saveNotes = (notes) => {
     fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 }
-var addNote = (title, body)=> {
-  var notes = fetchNotes();
-  var note = {
+let addNote = (title, body)=> {
+  let notes = fetchNotes();
+  let note = {
     title: title,
     body: body
   };
 
   //check the duplicate noteSting
-  var duplicateNotes = notes.filter((note)=> note.title === title);
+  let duplicateNotes = notes.filter((note)=> note.title === title);
   if(duplicateNotes.length === 0){
   notes.push(note)//pushing the note to the notes array
   saveNotes(notes);
   return note ;
 }
 };
-var getAll = () => {
+let getAll = () => {
   //console.log("Getting All the Nodes");
   return fetchNotes();
 }
-var readNote = (title) =>{
-  var notesRead = fetchNotes();
-  var readNotes = notesRead.filter((notesRead) => notesRead.title === title );
+let readNote = (title) =>{
+  let notesRead = fetchNotes();
+  let readNotes = notesRead.filter((notesRead) => notesRead.title === title );
   return readNotes[0];
 //  console.log('Reading  Note', title);
 }
-var removeNote = (title) => {
+let removeNote = (title) => {
 //  console.log ('Removing  Note', title);
 //fetch note
-var notes = fetchNotes();
-var filterNotes = notes.filter((note) => note.title !== title);
+let notes = fetchNotes();
+let filterNotes = notes.filter((note) => note.title !== title);
 if (notes.filter((note)=> note.title === title)){
   saveNotes(filterNotes);
 }
   return notes.length !== filterNotes.length;
 }
 
-var notesLog = (note) =>{
+let notesLog = (note) =>{
   debugger;
   console.log('----');
   console.log('Title :' + note.title);
